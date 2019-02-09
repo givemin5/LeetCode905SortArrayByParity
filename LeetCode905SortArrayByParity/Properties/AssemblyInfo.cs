6980fc8b5b6fbbbcf90a645c7f7df0d71b0acef3
givemin5 <givemin5@gmail.com>
2019-02-09 19:14:32 +0800

    Add project files.

---
+++ LeetCode905SortArrayByParity/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LeetCode905SortArrayByParity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode905SortArrayByParity")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3b443284-0370-411b-bd00-fc5d78912704")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LeetCode905SortArrayByParity/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode905SortArrayByParity
+{
+    public class Solution
+    {
+        public int[] SortArrayByParity(int[] A)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
